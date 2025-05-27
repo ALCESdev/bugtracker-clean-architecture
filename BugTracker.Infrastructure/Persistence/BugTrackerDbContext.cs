64b580444f267aa70193fae8bf4ef192b7655e4d
Alejandro Alcantara Escoda <alejandroalces@gmail.com>
2025-05-27 21:11:15 +0200

    Add Entity Framework Core support and DbContext setup
    - Updated `BugTracker.Infrastructure.csproj` to include
      `Microsoft.EntityFrameworkCore` version `9.0.5`.
    - Removed unnecessary `Class1.cs` file.
    - Added `BugTrackerDbContext.cs` to define the database
      context with `DbSet` properties for `User`, `Project`,
      and `Issue` entities.
    - Implemented `OnModelCreating` to configure entity
      relationships and delete behaviors.

---
+++ BugTracker.Infrastructure/Persistence/BugTrackerDbContext.cs
@@ -0,0 +1,34 @@
+﻿using BugTracker.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BugTracker.Infrastructure.Persistence;
+
+public class BugTrackerDbContext : DbContext
+{
+    public BugTrackerDbContext(DbContextOptions<BugTrackerDbContext> options) : base(options) { }
+
+    public DbSet<User> Users => Set<User>();
+    public DbSet<Project> Projects => Set<Project>();
+    public DbSet<Issue> Issues => Set<Issue>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>()
+            .HasMany(u => u.ReportedIssues)
+            .WithOne(i => i.Reporter)
+            .HasForeignKey(i => i.ReporterId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<User>()
+            .HasMany(u => u.AssignedIssues)
+            .WithOne(i => i.Assignee)
+            .HasForeignKey(i => i.AssigneeId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<Project>()
+            .HasMany(p => p.Issues)
+            .WithOne(i => i.Project)
+            .HasForeignKey(i => i.ProjectId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
