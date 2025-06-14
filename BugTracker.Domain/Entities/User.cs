ddfa225e7be81b2838a7ad521704631a461baf0d
Alex ALCES <alejandroalces@gmail.com>
2025-06-14 20:41:51 +0200

    Enhance User class with new properties
    Updated the `User` class in `User.cs` to include new properties: `PasswordHash`, `FullName`, `PhoneNumber`, `Address`, `UserRole`, `IsActive`, `CreatedAt`, and `ReportedIssues`. The `Email` property retains its validation attributes, while the `AssignedIssues` property remains unchanged. The `namespace` declaration has also been removed.

--- BugTracker.Domain/Entities/User.cs
+++ BugTracker.Domain/Entities/User.cs
@@ -1,8 +1,6 @@
 ﻿using BugTracker.Domain.Enums;
 using System.ComponentModel.DataAnnotations;
 
-namespace BugTracker.Domain.Entities;
-
 public class User
 {
     public Guid Id { get; private set; } = Guid.NewGuid();
@@ -15,7 +13,23 @@ public class User
     [MaxLength(100)]
     public string Email { get; set; } = string.Empty;
 
+    [Required]
+    public string PasswordHash { get; set; } = string.Empty;
+
+    [MaxLength(100)]
+    public string? FullName { get; set; }
+
+    [MaxLength(15)]
+    public string? PhoneNumber { get; set; }
+
+    [MaxLength(250)]
+    public string? Address { get; set; }
+
     public UserRole UserRole { get; set; }
+
+    public bool IsActive { get; set; } = true;
+
+    public DateTime CreatedAt { get; private set; } = DateTime.UtcNow;
 
     public List<Issue> ReportedIssues { get; set; } = [];
     public List<Issue> AssignedIssues { get; set; } = [];
